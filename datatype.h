aeb824b944e10b92f98933aba7cda4feacc7fdae
roy.lieu <roy.lieu@gmail.com>
2018-11-06 10:30:16 +0800

    add Cache class

--- datatype.h
+++ datatype.h
@@ -6,6 +6,7 @@
 #define CPPBLOG_DATATYPE_H
 
 #include <iostream>
+#include <unordered_map>
 
 struct article {
     int id;
@@ -14,5 +15,31 @@ struct article {
     std::string dummy_body;
     std::string real_body;
 };
+
+template <typename keyType, typename valueType>
+class Cache {
+public:
+    valueType Read(keyType key);
+    void Write(keyType key, valueType value);
+
+private:
+    std::unordered_map<keyType, valueType> _mapCache;
+};
+
+template <typename keyType, typename valueType>
+valueType Cache<keyType, valueType>::Read(keyType key) {
+    typename std::unordered_map<keyType, valueType>::const_iterator result = _mapCache.find(key);
+    if(result != _mapCache.end()) {
+        return result->second;
+    }
+}
+
+template <typename keyType, typename valueType>
+void Cache<keyType, valueType>::Write(keyType key, valueType value) {
+    if (_mapCache.find(key) != _mapCache.end()) {
+        _mapCache.erase(key);
+    }
+    _mapCache.insert(std::make_pair(key, value));
+}
 
 #endif //CPPBLOG_DATATYPE_H
