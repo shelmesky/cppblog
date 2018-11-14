59cdf5dd50cf8550b67aae2fe92bfef3700c1db2
roy.lieu <roy.lieu@gmail.com>
2018-11-14 19:50:00 +0800

    move main application to header file

--- main.cpp
+++ main.cpp
@@ -34,30 +34,6 @@ int initSOCIConnectionPool(std::string const& db_type, std::string const& db_con
 }
 
 
-class cppblog : public cppcms::application {
-public:
-    cppblog(cppcms::service &srv) :
-        cppcms::application(srv)
-    {
-        dispatcher().assign("/article/(\\d+)", &cppblog::show_article, this, 1);
-        mapper().assign("article", "/article/{1}");
-
-        dispatcher().assign("/", &cppblog::index, this);
-        mapper().assign("");
-
-        dispatcher().assign("/page/(\\d+)", &cppblog::index_page, this, 1);
-        mapper().assign("page", "/page/{1}");
-
-        mapper().root("/");
-    }
-    void index();
-    void show_article(std::string number);
-    void index_page(std::string number);
-    std::list<article> get_articles(int page);
-    article get_single_article(int id);
-};
-
-
 void cppblog::show_article(std::string number) {
     int no = std::stoi(number);
     const article& ret = get_single_article(no);
